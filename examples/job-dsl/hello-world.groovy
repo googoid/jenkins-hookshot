dd02986710d7c3b3770caa3ef217ab226a8ec9a3
Roger Ignazio <roger@puppetlabs.com>
2015-01-12 14:44:19 -0800

    add $CREATE_JOBS param to example Job DSL script

--- examples/job-dsl/hello-world.groovy
+++ examples/job-dsl/hello-world.groovy
@@ -1,23 +1,25 @@
 def basename = "${REPO_NAMESPACE}__${REPO_NAME}__${UNIQ_ID}"
 
-job {
-  // Ship it all to Logstash when the build completes
-  configure {project ->
-    project / publishers << 'jenkins.plugins.logstash.LogstashNotifier' {
-      maxLines(-1)
-      failBuild(true)
+if ("${CREATE_JOBS}".toBoolean()) {
+  job {
+    // Ship it all to Logstash when the build completes
+    configure {project ->
+      project / publishers << 'jenkins.plugins.logstash.LogstashNotifier' {
+        maxLines(-1)
+        failBuild(true)
+      }
     }
-  }
 
-  // Job config
-  name "${basename}__test-job"
-  description "${REPO_DESCRIPTION}"
-  label "mesos"
-  scm {
-    git("${REPO_URL}", "${GIT_SHA}")
-  }
-  steps {
-    shell('echo "Hello, world!"')
+    // Job config
+    name "${basename}__test-job"
+    description "${REPO_DESCRIPTION}"
+    label "mesos"
+    scm {
+      git("${REPO_URL}", "${GIT_SHA}")
+    }
+    steps {
+      shell('echo "Hello, world!"')
+    }
   }
 }
 
